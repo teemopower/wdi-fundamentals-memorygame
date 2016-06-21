30128e54e7a1f219d9923514541c8e14ac54e2f8
Jimmy Kim <jimmykim80@Jimmys-MacBook-Pro.local>
2016-06-21 01:09:07 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log(cardOne === cardTwo);
+console.log(cardOne === cardFour);
