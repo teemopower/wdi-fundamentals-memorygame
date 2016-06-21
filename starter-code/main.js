bf8b2d21680dd58ddc0f981ab236d17fa250bb9c
Jimmy Kim <jimmykim80@Jimmys-MacBook-Pro.local>
2016-06-21 12:04:56 -0700

    added conditionals

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,19 @@ var cardFour = "king";
 
 console.log(cardOne === cardTwo);
 console.log(cardOne === cardFour);
+
+
+
+if(cardTwo === cardFour){
+	alert("Sorry, try again.");
+} else if(cardTwo === cardThree){
+	alert("Sorry, try again.");
+} else if(cardOne === cardFour){
+	alert("Sorry, try again.");
+} else if(cardOne === cardThree){
+	alert("Sorry, try again.");
+} else if(cardOne === cardTwo){
+	alert("You found a match!");
+} else if(cardThree === cardFour){
+	alert("You found a match!");
+}
