fbb0e5acdc36fe782184f0692de8e85a26e07334
Jimmy Kim <jimmykim80@Jimmys-MacBook-Pro.local>
2016-06-23 11:26:56 -0700

    hw 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,9 @@
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-
-/*
 if(cardTwo === cardFour){
 	alert("Sorry, try again.");
 } else if(cardTwo === cardThree){
@@ -20,18 +19,50 @@ if(cardTwo === cardFour){
 }
 */
 
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
 var gameBoard = document.getElementById('game-board');
 
-
-
-
-
 var createBoard = function(){
-	for(var i=0; i < 4; i++){
+	for(var i=0; i < cards.length; i++){
 		var newCard = document.createElement('div');
 		newCard.className = 'card';
+
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards);
+
 		gameBoard.appendChild(newCard);
 	}
+}
+
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	
+	if(this.getAttribute('data-card') === "queen"){
+		this.innerHTML = '<img src="queen.png" alt="Queen of Heart" />';
+	} else {
+		this.innerHTML = '<img src="king.png" alt="King of Heart" />';
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+var isMatch = function(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+		
+	} else {
+		alert("Sorry, try again.");
+		
+	}
+
+	//document.getElementById("game-board").innerHTML = " ";
+	
 }
 
 createBoard();
